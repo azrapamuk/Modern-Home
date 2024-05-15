57fdc9b225db717496d986bd533dc92196d7f623
amillj <amilljaljak@hotmail.com>
2024-05-15 17:23:27 +0200

    dodan konekcijski string

--- ModernHome/obj/Debug/net8.0/ModernHome.AssemblyInfo.cs
+++ ModernHome/obj/Debug/net8.0/ModernHome.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModernHome")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d068b0a5d4b102fa1f794294e6e477bb1fb8230")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5faeb5a67bd7198e999d59262434b238efabfd16")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModernHome")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModernHome")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
